628813064a854a5e7cbbe8a48b5ba113c32f4dee
Pedro Henrique Xavier <ped-x@hotmail.com>
2019-09-28 20:35:19 -0300

    Update ElementoTabelaSimbolo.java

--- compiladores/ElementoTabelaSimbolo.java
+++ compiladores/ElementoTabelaSimbolo.java
@@ -1,15 +1,26 @@
 package compiladores;
 
 public class ElementoTabelaSimbolo {
-	Token token;
+	//Token token;
+	byte token_;
 	String lexema;	
 	
-	public ElementoTabelaSimbolo(Token token, String lexema) {
-		this.token = token;
+	public ElementoTabelaSimbolo(byte token_,  String lexema) {
+		//this.token = token;
+		this.token_=token_;
 		this.lexema = lexema;
 	}	
 	
 	public String getLexema() {
 		return lexema;
+	}
+	public void setLexema(String lexema){
+		this.lexema = lexema;	
+	}
+	public byte getToken() {
+		return token_;
+	}
+	public void setToken(byte token_){
+		this.token_ = token_;	
 	}
 }
